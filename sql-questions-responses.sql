5db2750e13d19538b17b199b755ae1f2c263cb9e
= <=>
2024-11-05 17:50:24 -0500

    attempt to get all tasks belonging to user regardless of permission. ambiguous column name parse error

--- sql-questions-responses.sql
+++ sql-questions-responses.sql
@@ -1,9 +1,10 @@
 -- 1.) Find all tasks that a user (identified by user_id) has access to but does not have permission to edit.
 
 --  -- By "has access to", I assume that means "created" since the only options are "created_by" and "can_edit"
-select task_id, user_id
-from users, tasks
-where users.user_id = tasks.created_by;
+select task_id, user_id, permissions.can_edit
+from users, tasks, permissions
+where users.user_id = tasks.created_by and
+permissions.user_id = tasks.created_by;
 
 -- 2.) Find all users who have created tasks but do not have permission to edit them.
 
