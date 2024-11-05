65e8901877732bf955f353dc268f86b7a7c5041f
= <=>
2024-11-05 17:45:40 -0500

    initial commit, breaking down question 1

---
+++ sql-questions-responses.sql
@@ -0,0 +1,10 @@
+-- 1.) Find all tasks that a user (identified by user_id) has access to but does not have permission to edit.
+
+--  -- By "has access to", I assume that means "created" since the only options are "created_by" and "can_edit"
+select task_id, user_id
+from users, tasks
+where users.user_id = tasks.created_by;
+
+-- 2.) Find all users who have created tasks but do not have permission to edit them.
+
+-- Bonus: Write an SQL script that restores edit permissions for a user if they have created a task but lost the edit permission.
