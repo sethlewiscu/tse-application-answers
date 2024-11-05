aebc1b00fd84b2ade35e9f45f87e76dceb7e41b7
= <=>
2024-11-05 17:53:36 -0500

    draft of full query question #1

--- sql-questions-responses.sql
+++ sql-questions-responses.sql
@@ -1,10 +1,11 @@
 -- 1.) Find all tasks that a user (identified by user_id) has access to but does not have permission to edit.
 
 --  -- By "has access to", I assume that means "created" since the only options are "created_by" and "can_edit"
-select tasks.task_id, users.user_id, permissions.can_edit
+select tasks.task_id, users.user_id, users.name, permissions.can_edit
 from users, tasks, permissions
 where users.user_id = tasks.created_by and
-permissions.user_id = tasks.created_by;
+permissions.user_id = tasks.created_by and
+permissions.can_edit = 1;
 
 -- 2.) Find all users who have created tasks but do not have permission to edit them.
 
